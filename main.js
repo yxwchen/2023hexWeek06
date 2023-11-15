23678154010fd57353bd111c81e845e9fb123b2a
claire <claire1212@gmail.com>
2023-11-15 15:03:42 +0800

    finish Week06

---
+++ main.js
@@ -0,0 +1,47 @@
+// LV2============================
+const elList = document.querySelector('.ticketCard-area');
+let data = [];
+
+axios.get('https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json')
+    .then((response) => {
+        // console.log(response.data.data); //測試拿到全部資料
+        data = response.data.data;
+        render();
+    });
+
+function render() {
+    let str = '';
+    data.forEach(function (item) {
+        str += `
+                <li class="ticketCard">
+                    <div class="ticketCard-img">
+                        <a href="#">
+                            <img src="${item.imgUrl}" alt="">
+                        </a>
+                        <div class="ticketCard-region">${item.area}</div>
+                        <div class="ticketCard-rank">${item.rate}</div>
+                    </div>
+                    <div class="ticketCard-content">
+                        <div>
+                            <h3>
+                                <a href="#" class="ticketCard-name">${item.name}</a>
+                            </h3>
+                            <p class="ticketCard-description">
+                            ${item.description}
+                            </p>
+                        </div>
+                        <div class="ticketCard-info">
+                            <p class="ticketCard-num">
+                                <span><i class="fas fa-exclamation-circle"></i></span>
+                                剩下最後 <span id="ticketCard-num">${item.group} </span> 組
+                            </p>
+                            <p class="ticketCard-price">
+                                TWD <span id="ticketCard-price">$${item.price}</span>
+                            </p>
+                        </div>
+                    </div>
+                </li>
+`
+    });
+    elList.innerHTML = str;
+}
